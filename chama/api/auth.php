0c4da4af3f66a4897cdb16747442ee15b88e0dbc
Sedrick Otolo - Kakebe <sedrick@kakebetechcamp.tech>
2026-07-02 17:03:06 +0300

    login tried to fix

--- chama/api/auth.php
+++ chama/api/auth.php
@@ -6,7 +6,7 @@
 
 if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/db/connection.php';
+$conn = require_once __DIR__ . '/../db/connection.php';
 
 // Set JSON response header
 header('Content-Type: application/json');
